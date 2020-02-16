0e8d89106cd55c791b82733f5a2529e0d55ca57b
Rafael Bernabeu <rbbernabeu@gmail.com>
2020-02-16 17:24:54 -0300

    add constructor + updates

--- src/main/java/br/bernabeu/RecursiveTasker.java
+++ src/main/java/br/bernabeu/RecursiveTasker.java
@@ -1,3 +1,5 @@
+package br.bernabeu;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -8,16 +10,20 @@ import java.util.function.Consumer;
 
 public class RecursiveTasker<T> extends RecursiveTask<Collection<T>> {
 
-    private final ForkJoinPool pool;
     private final List<T> tasks;
     private final int start;
     private final int end;
     private final Consumer<T> itemAction;
     private final BinaryOperator<Collection<T>> groupAction;
+    private ForkJoinPool pool;
     private int splitValue;
 
     public RecursiveTasker(Collection<T> tasks, Consumer<T> itemAction) {
-        this(tasks, 0, tasks.size(), itemAction, (x, y) -> { x.addAll(y); return x; }, getSplitValue(tasks), null);
+        this(tasks, itemAction, (ForkJoinPool) null);
+    }
+
+    public RecursiveTasker(Collection<T> tasks, Consumer<T> itemAction, ForkJoinPool pool) {
+        this(tasks, 0, tasks.size(), itemAction, (x, y) -> { x.addAll(y); return x; }, getSplitValue(tasks), pool);
     }
 
     public RecursiveTasker(Collection<T> tasks, Consumer<T> itemAction, BinaryOperator<Collection<T>> groupAction) {
@@ -25,7 +31,7 @@ public class RecursiveTasker<T> extends RecursiveTask<Collection<T>> {
     }
 
     private RecursiveTasker(Collection<T> tasks, int start, int end, Consumer<T> itemAction, BinaryOperator<Collection<T>> groupAction, int splitValue, ForkJoinPool pool) {
-        this.pool = pool == null ? new ForkJoinPool() : pool;
+        this.pool = pool;
         this.tasks = new ArrayList<>(tasks);
         this.start = start;
         this.end = end;
@@ -35,27 +41,15 @@ public class RecursiveTasker<T> extends RecursiveTask<Collection<T>> {
     }
 
     private static <T> int getSplitValue(Collection<T> tasks) {
-        int listSize = tasks.size();
-        int cores = Runtime.getRuntime().availableProcessors();
-        if (listSize >= 1000) {
-            return (listSize / (cores * 8)) + 1;
-        } else if (listSize >= 100) {
-            return (listSize / (cores * 4)) + 1;
-        } else {
-            return (listSize / (cores * 2)) + 1;
-        }
+        int r = tasks.size() / (Runtime.getRuntime().availableProcessors() * 2);
+        return r > 0 ? r : 1;
     }
 
     @Override
     protected Collection<T> compute() {
         if (end - start <= splitValue) {
             for (int i = start; i < end; i++) {
-                try {
-                    itemAction.accept(tasks.get(i));
-                } catch (Exception e) {
-                    pool.shutdownNow();
-                    throw e;
-                }
+                itemAction.accept(tasks.get(i));
             }
             return tasks.subList(start, end);
         } else {
@@ -67,12 +61,15 @@ public class RecursiveTasker<T> extends RecursiveTask<Collection<T>> {
         }
     }
 
-    public Collection<T> start() {
-        return pool.invoke(this);
-    }
-
     public Collection<T> start(int splitValue) {
         this.splitValue = splitValue;
+        return this.start();
+    }
+
+    public Collection<T> start() {
+        if (pool == null) {
+            pool = ForkJoinPool.commonPool();
+        }
         return pool.invoke(this);
     }
 }
---
+++ src/test/java/br/bernabeu/RecursiveTaskerTest.java
@@ -0,0 +1,65 @@
+package br.bernabeu;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RunWith(JUnit4.class)
+public class RecursiveTaskerTest {
+
+    private static final BinaryOperator<Collection<Integer>> INTEGER_REDUCER = (x, y) -> Arrays.asList(x.stream().reduce((a, b) -> a + b).get() +
+            y.stream().reduce((a, b) -> a + b).get());
+
+    @Test
+    public void testarConstrutor() throws Exception {
+        new RecursiveTasker<>(new HashSet<>(), t -> {}).start();
+        new RecursiveTasker<>(new ArrayList<>(), t -> {}).start();
+        new RecursiveTasker<>(new TreeSet<>(), t -> {}).start();
+    }
+
+    @Test
+    public void testarPopulandoUmaLista() throws Exception {
+        String value = "0";
+        List<String> values = Stream.iterate(value, x -> x).limit(10000).collect(Collectors.toList());
+        new RecursiveTasker<>(values, x -> Assert.assertEquals(value, x)).start();
+    }
+
+    @Test
+    public void testarSomandoOsValoresDeUmaLista() throws Exception {
+        //0+1+2+3+4+5+6+7+8+9
+        List<Integer> values = Stream.iterate(0, x -> x + 1).limit(10).collect(Collectors.toList());
+        Collection<Integer> result = new RecursiveTasker<>(values, x -> {}, INTEGER_REDUCER).start();
+        Assert.assertEquals(0+1+2+3+4+5+6+7+8+9, result.stream().findFirst().get().intValue());
+    }
+
+    @Test
+    public void testarQueEhMaisRapidoMesmo() throws Exception {
+        List<Integer> values = Stream.iterate(0, x -> x + 1).limit(100).collect(Collectors.toList());
+        Consumer<Integer> consumer = t -> {
+            try {
+                Thread.sleep(1);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+        long ti1,ti2,tf1,tf2 = 0;
+        ti1 = System.currentTimeMillis();
+        values.forEach(consumer);
+        tf1 = System.currentTimeMillis();
+
+        ti2 = System.currentTimeMillis();
+        Collection<Integer> result = new RecursiveTasker<>(values, consumer).start();
+        tf2 = System.currentTimeMillis();
+
+        System.out.println("SingleThread: " + (tf1-ti1));
+        System.out.println("MultiThread : " + (tf2-ti2));
+        Assert.assertTrue((tf1-ti1) > (tf2-ti2));
+    }
+}
